1e8359c2795c91492f282309bcd143a67fb1d8f6
Ashley <ashleybarnes.astro@gmail.com>
2019-11-22 12:14:50 +0100

    Fixed several errors when input contains nan values

--- bindata.py
+++ bindata.py
@@ -4,15 +4,17 @@ import numpy as np
 def sigma_err(sigma_data, oversample):
 
     '''Calculate uncertainy for bin considering only y data'''
+    id_nonnan = ~np.isnan(sigma_data)
+    sigma_data_ = sigma_data[id_nonnan]
+    n = len(sigma_data_)
 
-    n = len(sigma_data)
-
-    sigma_err_var = np.sqrt(np.nansum(sigma_data**2 / n))
+    sigma_err_var = np.sqrt(np.nansum(sigma_data_**2 / n))
     sigma_err_mean = sigma_err_var / np.sqrt(n / oversample)
     sigma_err_median = sigma_err_mean * 1.25
 
     return sigma_err_mean, sigma_err_median
 
+#not used yet...
 def sigma_err_2d(data1_err, data2_err):
 
     '''Calculate uncertainy for bin considering both x and y data'''
@@ -33,8 +35,8 @@ def get_bins_1d(data1, data2, data1_err, data2_err, oversample=1, bins='', nbins
         INPUT:
             data1 = x-axis data - accepts multi-dimentional arrays
             data2 = y-axis data - accepts multi-dimentional arrays
-            data1_err = x-axis data err
-            data2_err = x-axis data err
+            data1_err = x-axis data err (not used)
+            data2_err = y-axis data err
             oversample = data oversampling ratio, accounting that data points oversample beam, and are therefore correlated and not independent measuremnets- usually N_oversample = 1.13 * beam_area / pixel_area; Default = 1
             bins = bin ranges, i.e. edges of bins; Default = determine bins from min and max x-data values
             nbins = if bins not defined, then number of bins to deteremine; Default = 10
@@ -55,6 +57,19 @@ def get_bins_1d(data1, data2, data1_err, data2_err, oversample=1, bins='', nbins
     data2 = data2.flatten()
     data1_err = data1_err.flatten()
     data2_err = data2_err.flatten()
+    
+    #Remove nan values in data2 -> may screw with stats!
+    id_nonnan = np.where(~np.isnan(data2))
+    data1 = data1[id_nonnan]
+    data2 = data2[id_nonnan]
+    data1_err = data1_err[id_nonnan]
+    data2_err = data2_err[id_nonnan]
+    
+    id_nonnan = np.where(~np.isnan(data2_err))
+    data1 = data1[id_nonnan]
+    data2 = data2[id_nonnan]
+    data1_err = data1_err[id_nonnan]
+    data2_err = data2_err[id_nonnan]
 
     if bins=='':
         min=np.nanmin(data1)
@@ -76,7 +91,10 @@ def get_bins_1d(data1, data2, data1_err, data2_err, oversample=1, bins='', nbins
     significant = np.empty([nbins]) *np.nan
     neg = np.empty([nbins]) *np.nan
     pos = np.empty([nbins]) *np.nan
-
+    ntot = np.empty([nbins]) *np.nan
+    isnan = np.empty([nbins]) *np.nan
+    isnotnan = np.empty([nbins]) *np.nan
+    
     for i in range(nbins):
 
         ids_ = (data1 > bins[i]) & (data1 < bins[i+1])
@@ -95,14 +113,40 @@ def get_bins_1d(data1, data2, data1_err, data2_err, oversample=1, bins='', nbins
 
         neg[i] = len(np.where(data2[ids]<=0)[0])
         pos[i] = len(np.where(data2[ids]>0)[0])
-
+        ntot[i] = len(data2[ids])
+        isnan[i] = len(np.where(np.isnan(data2[ids]))[0])
+        isnotnan[i] = len(np.where(~np.isnan(data2[ids]))[0])
+        
+        if isnan[i] > 0: 
+            print('[Warning] Bin contains nan values that should have been taken care of!')
+            
     id_ = ~np.isnan(significant)
     significant = significant[id_]
+    sigma = sigma[id_]
     p1 = p1[id_]
     p2 = p2[id_]
     x = x[id_]
     y = y[id_]
+    ntot = ntot[id_]
+    isnan = isnan[id_]
+    isnotnan = isnotnan[id_]
+    neg = neg[id_]
+    pos = pos[id_]
+    
+    keys = ['significance', 'ybin_err', '-1sigma', '+1sigma', 'nneg', 'npos', 'ntot', 'isnotnan', 'isnan']
 
-    stats = np.array([significant, p1, p2, neg, pos])
+    stats = dict.fromkeys(keys)
+
+    stats['significance'] = significant
+    stats['ybin_err'] = sigma
+    stats['-1sigma'] = p1
+    stats['+1sigma'] = p2
+    stats['nneg'] = neg
+    stats['npos'] = pos
+    stats['ntot'] = ntot
+    stats['isnan'] = isnan
+    stats['isnotnan'] = isnotnan
+    
+#     stats = np.array([significant, p1, p2, neg, pos])
 
     return x, y, stats, bins
